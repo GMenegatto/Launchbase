2f8bd9a9a8c4729f95a7e493550baedae4a2f509
GMenegatto <gabrielmenegattogf@hotmail.com>
2020-07-05 01:07:07 -0300

    launchbase

---
+++ fase3/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res){
+    return res.render("about");
+});
+
+server.get("/aulas", function(req, res){
+    return res.render("aulas");
+});
+
+
+server.listen(5000, function(){
+    console.log('server is running');
+});
